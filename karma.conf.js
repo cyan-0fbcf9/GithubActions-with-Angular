c54dcab9cffb86dadb13d16ef6ce2c3a783f1630
cyan <namid224y@gmail.com>
2020-08-14 23:21:58 +0900

    update blank.yml

--- karma.conf.js
+++ karma.conf.js
@@ -25,8 +25,8 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
   });
 };
